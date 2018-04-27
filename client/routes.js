8068160a29ecf84df3e8815c08b5baa07c9bab53
Josh Remaley <joshremaley@gmail.com>
2018-04-27 11:15:33 -0500

    get tstat route working

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome, Thermostat, Schedule, Settings} from './components'
-import {me} from './store'
+import {me, getThermostat} from './store'
 
 /**
  * COMPONENT
@@ -53,7 +53,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     loadInitialData () {
-      dispatch(me())
+      dispatch(getThermostat())
     }
   }
 }
--- client/store/thermostat.js
+++ client/store/thermostat.js
@@ -5,7 +5,6 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_TSTAT = 'GET_TSTAT'
-const UPDATE_SCHEDULE = 'UPDATE_SCHEDULE'
 
 /**
  * INITIAL STATE
@@ -16,36 +15,15 @@ const defaultStat = {}
  * ACTION CREATORS
  */
 const getTstat = stat => ({type: GET_TSTAT, stat})
-const putSched = () => ({type: UPDATE_SCHEDULE})
 
 /**
  * THUNK CREATORS
  */
 export const getThermostat = () =>
   dispatch =>
-    axios.get('/api/')
+    axios.get('/api/thermostat/')
       .then(res =>
         dispatch(getTstat(res.data || defaultStat)))
-      .catch(err => console.log(err))
-
-export const auth = (email, password, method) =>
-  dispatch =>
-    axios.post(`/auth/${method}`, { email, password })
-      .then(res => {
-        dispatch(getUser(res.data))
-        history.push('/home')
-      }, authError => { // rare example: a good use case for parallel (non-catch) error handler
-        dispatch(getUser({error: authError}))
-      })
-      .catch(dispatchOrHistoryErr => console.error(dispatchOrHistoryErr))
-
-export const logout = () =>
-  dispatch =>
-    axios.post('/auth/logout')
-      .then(_ => {
-        dispatch(removeUser())
-        history.push('/login')
-      })
       .catch(err => console.log(err))
 
 /**
@@ -55,8 +33,6 @@ export default function (state = defaultStat, action) {
   switch (action.type) {
     case GET_TSTAT:
       return action.stat
-    case REMOVE_USER:
-      return defaultUser
     default:
       return state
   }
